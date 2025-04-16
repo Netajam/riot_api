a8924c280964011e323bedafd1a2701142a0006e
Parrhasios <mike.reynard23@gmail.com>
2025-04-16 14:44:45 +0200

    Hmac key in config

--- lib/riot_api_web/controllers/crypto_controller.ex
+++ lib/riot_api_web/controllers/crypto_controller.ex
@@ -2,9 +2,15 @@ defmodule RiotApiWeb.CryptoController do
   use RiotApiWeb, :controller
   require Logger
   alias RiotApi.Crypto
-  
+
   def sign(conn, params) do
-    secret_key = "TEMPORARY_SECRET"
-    signature = Crypto.sign(params, secret_key)
-    json(conn, %{signature: signature})  end
+    secret_key = Application.get_env(:riot_api, :hmac_secret)
+    unless secret_key do
+      Logger.error("HMAC Secret Key is not configured!")
+      send_resp(conn, :internal_server_error, "{\"error\": \"Server configuration error\"}")
+    else
+      signature = Crypto.sign(params, secret_key)
+      json(conn, %{signature: signature})
+    end
+  end
 end
