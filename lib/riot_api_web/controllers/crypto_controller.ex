1e69497caf4fd9b09d4a01d99e7b1a57ee7615cc
Parrhasios <mike.reynard23@gmail.com>
2025-04-16 15:14:00 +0200

    sign function with input validation

--- lib/riot_api_web/controllers/crypto_controller.ex
+++ lib/riot_api_web/controllers/crypto_controller.ex
@@ -2,8 +2,9 @@ defmodule RiotApiWeb.CryptoController do
   use RiotApiWeb, :controller
   require Logger
   alias RiotApi.Crypto
-
-  def sign(conn, params) do
+  plug :accepts, ["json"] #check already performed in router.ex but in case router accept other types in the future
+  # region Sign function
+  def sign(conn, params) when is_map(params) do
     secret_key = Application.get_env(:riot_api, :hmac_secret)
     unless secret_key do
       Logger.error("HMAC Secret Key is not configured!")
@@ -13,4 +14,13 @@ defmodule RiotApiWeb.CryptoController do
       json(conn, %{signature: signature})
     end
   end
+  # Case where we don't receive a map from our Parser
+  def sign(conn, params) do
+    Logger.error("Invalid payload format for sign action: Expected a JSON object (map), received: #{inspect(params)}")
+    conn
+    |> put_status(:bad_request)
+    |> json(%{error: "Invalid payload format. Expected a JSON object."})
+  end
+  # endregion
+
 end
